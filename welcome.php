7588a61d98de390b30e1b6a119a5c8bb5ebc33e7
alokkumarak <alok.sonaili@gmail.com>
2020-10-06 21:42:14 +0530

    for a contest

--- welcome.php
+++ welcome.php
@@ -23,7 +23,10 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !=true)
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <title>PHP-REGISTRATION</title>
-    <link rel="stylesheet" href="style.css">
+<!--     <link rel="stylesheet" href="style.css"> -->
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
   </head>
   <body >
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -68,46 +71,39 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !=true)
 <h1 align="center" ><b>LOGGED-IN</b></h1><hr>
 
 
+        <div id="container">
+           <div class="page_content">
+              if you have food then don't waste send it here....
+    </div>
+    <div class="comment_input">
+        <form name="form1" >
+           <div class="form-group col-md-11">
+          <input type="text" name="name" placeholder="Name..." class="form-control"/>   
+            </div>
+              <div class="form-group col-md-11">
+            <input type="email" name="email" placeholder="Email....." class="form-control" />
+          </div>
+          <div class="form-group col-md-11">
+            <input type="number" name="num" placeholder="Your contact number....." class="form-control" />
+          </div>
+          <div class="form-group col-md-11">
+           <input type="text" name="comments" placeholder="Leave Food details Here....." class="form-control" />
+          </div>
+              <div class="form-group col-md-11">
+            <textarea type="text" name="address" placeholder="Enter your full Address With pin no. " class="form-control" ></textarea>
+          </div>
+          <div class="form-group col-md-11">
+            <button  name="location" class="btn btn-info" value="add your location" >add your location</button> 
+          </div>
+            <a href="#" onClick="commentSubmit()" class="button btn btn-success" >Post</a>  &nbsp; &nbsp;  &nbsp;  &nbsp;
+            <input type="reset" value="reset">
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        </form>
+    </div>
+    <div id="comment_logs">
+      Loading comments...
+    <div>
+</div>
 
 
 
@@ -119,5 +115,44 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !=true)
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>
+  function commentSubmit(){
+    if(form1.name.value == '' || form1.comments.value == ''  ){ //exit if one of the field is blank
+      alert('Enter your complete details !');
+      return;
+    }
+    if(form1.email.value == '' || form1.num.value == ''){ //exit if one of the field is blank
+      alert('Enter your complete details !');
+      return;
+     }
+     if(form1.address.value == ''){ //exit if one of the field is blank
+        alert('Enter your complete details !');
+          return;
+    }
+    var name = form1.name.value;
+    var comments = form1.comments.value;
+    var email = form1.email.value;
+    var num = form1.num.value;
+    var address = form1.address.value;
+    var xmlhttp = new XMLHttpRequest(); //http request instance
+    
+    xmlhttp.onreadystatechange = function(){ //display the content of insert.php once successfully loaded
+      if(xmlhttp.readyState==4&&xmlhttp.status==200){
+        document.getElementById('comment_logs').innerHTML = xmlhttp.responseText; //the chatlogs from the db will be displayed inside the div section
+      }
+    }
+    xmlhttp.open('GET', 'insert.php?name='+name+'&comments='+comments+'&email='+email+'&num='+num+'&address='+address, true); //open and send http request
+    xmlhttp.send();
+  }
+  
+    $(document).ready(function(e) {
+      $.ajaxSetup({cache:false});
+      setInterval(function() {$('#comment_logs').load('logs.php');}, 2000);
+    });
+    
+</script>
+
   </body>
 </html>
